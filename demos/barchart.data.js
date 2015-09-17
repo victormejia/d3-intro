73429d20d303880ecff5ee45a6a9404336a83cf6
Victor Mejia <victor.a.mejia@gmail.com>
2015-09-17 08:42:58 -0700

    Auto-generated commit

---
+++ demos/barchart.data.js
@@ -0,0 +1,17 @@
+var data =
+[{"lang":"JavaScript","value":791535},
+{"lang":"Java","value":561366},
+{"lang":"Ruby","value":479525},
+{"lang":"CSS","value":354354},
+{"lang":"PHP","value":346761},
+{"lang":"Python","value": 317085},
+{"lang":"C","value":289880},
+{"lang":"C++","value":164722},
+{"lang":"C#","value":123509},
+{"lang":"Objective-C","value":119345},
+{"lang":"Shell","value":99555},
+{"lang":"Perl","value":73431},
+{"lang":"CoffeeScript","value":43541},
+{"lang":"R","value":42070},
+{"lang":"Go","value":38130}
+];
